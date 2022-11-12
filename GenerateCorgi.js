370c3d8b106a9ff42c2a7805eeec392b7b778b84
ivliu816 <ivliu816@gmail.com>
2022-11-12 01:33:27 -0500

    Added pictures to array

--- GenerateCorgi.js
+++ GenerateCorgi.js
@@ -1,4 +1,4 @@
-let images = ["Corgi_Fall.jpg", "Corgi_Toast.png"];
+let images = ["Corgi_Fall.jpg", "Corgi_Toast.png", "Corgi_Blanket.jpeg", "Corgi_Eggtoast.png", "Corgi_Leash.jpeg", "Corgi_Run.jpeg"];
 const img = "Image/" + images[Math.floor(Math.random()*images.length)];
 const corgi = document.getElementById("pic")
 corgi.src = img;
