54cced1062a70a0e9ea9232e370677723a3dce5e
Mike <mike.b.perreault@gmail.com>
2022-11-12 08:12:24 -0500

    got working color array (not super well tested)2

--- GenerateCorgi.js
+++ GenerateCorgi.js
@@ -1,21 +1,41 @@
 let images = ["Corgi_Fall.jpg", "Corgi_Toast.png", "Corgi_Blanket.jpeg", "Corgi_Eggtoast.png", "Corgi_Leash.jpeg", "Corgi_Run.jpeg"];
 const img = "Image/" + images[Math.floor(Math.random()*images.length)];
 const corgi = document.createElement('img');
+const canvasImg= document.getElementById('a');
+const pic = document.getElementById("pic");
+const width = 512;
+const height = 512;
+pic.src = img;
 corgi.src = img;
+const c = document.getElementById("circle");
+drawCanvas();
+async function drawCanvas() {
+    // const c = document.getElementById("circle")
+    const colorArray = await colors();
+    console.log(colorArray);
+    // let circleLayer = 0;
+    matrixCircle(Math.floor(width/2),colorArray[0]);
 
-const c = document.getElementById("circle")
-const ctx = c.getContext("2d");
+    // c.onmousemove(() => {
+    //     circleLayer +=1;
+    //     c.getContext("2d").clearRect(0, 0, c.width, c.height);
+    //     matrixCircle(Math.floor(width/Math.pow(2,circleLayer)),colorArray[circleLayer]);
+    // });
+}
+
 function drawCircle(x, y, r, color) {
+    const ctx = c.getContext("2d");
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2*Math.PI);
-    ctx.fillStyle(color);
+    ctx.fillStyle=color;
     ctx.fill();
 }
 
 function matrixCircle(r, color) {
-    for(var x = 0; x < Math.floor(c.width/r); x++) {
-        for(var y = 0; y < Math.floor(c.height/r); y++) {
-            drawCircle(x*r, y*r, r, convertRGBtoHex(color[x][y]));
+    for(var x = 0; x < Math.floor(c.width/(2*r)); x++) {
+        for(var y = 0; y < Math.floor(c.height/(2*r)); y++) {
+            console.log(r);
+            drawCircle((2*x+1)*r, (2*y+1)*r, r, convertRGBtoHex(color[x][y]));
         }
     }
 }
@@ -56,8 +76,8 @@ function decToHexa(n)
 function convertRGBtoHex(color)
 {
     R = color[0];
-    B = color[1];
-    G = color[2];
+    G = color[1];
+    B = color[2];
     if ((R >= 0 && R <= 255) && (G >= 0 && G <= 255) && (B >= 0 && B <= 255)) {
         let hexCode = "#";
         hexCode += decToHexa(R);
@@ -79,10 +99,10 @@ async function colors() {
             let rowColors = []
             for (let j = 0; j < Math.pow(2,width); ++j) {
                 let rgb = [0, 0, 0];
-                colorArray[width + 1][2 * i][2 * j].forEach((e, i) => rgb[i] += e);
-                colorArray[width + 1][2 * i][2 * j + 1].forEach((e, i) => rgb[i] += e);
-                colorArray[width + 1][2 * i + 1][2 * j].forEach((e, i) => rgb[i] += e);
-                colorArray[width + 1][2 * i + 1][2 * j + 1].forEach((e, i) => rgb[i] += e);
+                colorArray[width + 1][2 * i][2 * j].forEach((e, ind) => rgb[ind] += e);
+                colorArray[width + 1][2 * i][2 * j + 1].forEach((e, ind) => rgb[ind] += e);
+                colorArray[width + 1][2 * i + 1][2 * j].forEach((e, ind) => rgb[ind] += e);
+                colorArray[width + 1][2 * i + 1][2 * j + 1].forEach((e, ind) => rgb[ind] += e);
                 rgb = rgb.map(e => Math.floor(e / 4));
                 rowColors.push(rgb);
             }
@@ -94,17 +114,13 @@ async function colors() {
     async function calcSmallestCircleColors() {
 
         await new Promise((resolve) => { corgi.onload = resolve; });
-        console.log('loaded');
         const circles = Math.pow(2, layers)
         let colorArray = [];
-        const width = corgi.width;
-        const height = corgi.height;
-        const canvas = document.getElementById('c');
-        canvas.width = width;
+        const canvas = document.createElement("canvas");
         canvas.height = height;
+        canvas.width = width;
         const corgiCanvas = canvas.getContext('2d', 'willReadFrequently');
-        corgiCanvas.drawImage(corgi, 0, 0);
-        console.log('drew');
+        await corgiCanvas.drawImage(corgi, 0, 0, 512, 512);
         for (let i = 0; i < circles; ++i) {
             let lineColor = []
             const xoffset = Math.floor(i * width / circles);
@@ -113,10 +129,13 @@ async function colors() {
                 let rgb = [0, 0, 0];
 
                 let rgba = corgiCanvas.getImageData(
-                    xoffset, yoffset, width/circles, height/circles
+                    xoffset, yoffset, Math.floor(width/circles), Math.floor(height/circles)
                 ).data;
-                rgb = rgb.map((e,i) => rgba.reduce((acc,el, ind) => ind % 4 === i ? acc + el: acc, 0))
-                rgb = rgb.map(e => Math.floor(e /= Math.pow(Math.floor(corgi.width / circles), 2)));
+                if (i===10&&j===10) {
+                    console.log(rgba);
+                }
+                rgb = rgb.map((e,index) => rgba.reduce((acc,el, ind) => ind % 4 === index ? acc + el: acc, 0))
+                rgb = rgb.map(e => Math.floor(4*e/rgba.length));
                 lineColor.push(rgb);
             }
             colorArray.push(lineColor);
@@ -133,5 +152,4 @@ async function colors() {
     return await calcSmallestCircleColors().then(array => circleColors[layers] = array).then(() => doRest(circleColors));
 
 }
-console.log(colors());
 
