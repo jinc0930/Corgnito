907bcc2d7a13d188bd23504000dcd9881fcc6242
ivliu816 <ivliu816@gmail.com>
2022-11-12 01:21:27 -0500

    Changed Back

--- GenerateCorgi.js
+++ GenerateCorgi.js
@@ -1,3 +1,3 @@
 let images = ["Corgi_Fall.jpg", "Corgi_Toast.png"];
-const img = "Image/" + images[Math.ceil(Math.random()*images.length)];
+const img = "Image/" + images[Math.floor(Math.random()*images.length)];
 document.getElementById("pic").src = img;
