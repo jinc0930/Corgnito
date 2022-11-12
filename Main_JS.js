2f5de291c81c01e1f998c19ff8fa026b31af4e99
LastSpot <46937389+LastSpot@users.noreply.github.com>
2022-11-12 01:02:53 -0500

    Update

--- Main_JS.js
+++ Main_JS.js
@@ -12,4 +12,3 @@ const express = require("express");
 const corgi = new Image();
 let images = ["Corgi_Fall.jpg", "Corgi_Toast.png"];
 corgi.src = "Image/" + images[Math.floor(Math.random()*images.length)];
-
