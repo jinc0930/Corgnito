fb55d18604879c198cf9fd1e87593a27a4856634
ivliu816 <ivliu816@gmail.com>
2022-11-12 00:37:51 -0500

    Changes array elements

--- Main_JS.js
+++ Main_JS.js
@@ -1,3 +1,3 @@
 const corgi = new Image();
-let images = ["Corgi_Fall.jpg", "Corgi_Toast.png"];
-corgi.src = "Image/" + images[Math.floor(Math.random()*images.length)];
+let images = ["Image/Corgi_Fall.jpg", "Image/Corgi_Toast.png"];
+corgi.src = images[Math.floor(Math.random()*images.length)];
